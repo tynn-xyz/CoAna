5afb92174d2d6d529d28bb00bbd5476f809f4a9c
Christian Schmitz <christian@tynn.xyz>
2019-08-30 21:14:32 +0200

    Prepare example application

---
+++ example/src/main/kotlin/xyz/tynn/coana/example/ExampleActivity.kt
@@ -0,0 +1,11 @@
+package xyz.tynn.coana.example
+
+import android.app.Activity
+import android.os.Bundle
+
+class ExampleActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+    }
+}
